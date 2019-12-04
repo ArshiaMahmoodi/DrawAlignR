af0918c5fed773fa57dc60fc0c02e6aedeed52b0
Arshia Mahmoodi <arshia.mahmoodi@mail.utoronto.ca>
2019-12-04 15:29:05 -0500

    roxygen2 tags

--- R/runDrawAlignRShiny.R
+++ R/runDrawAlignRShiny.R
@@ -1,12 +1,36 @@
 #' Launches the shiny app for DrawAlignR - User facing function.
 #'
-#'
 #' @return None. Runs the Shiny webapp found in inst/available-shiny-apps/DrawAlignR/app.R
 #'
-#'
-#'
-#' @export
 #' @import shiny
+#' @import ggplot2
+#' @import data.table
+#' @import plyr
+#' @import tibble
+#' @import ggplot2
+#' @import gridExtra
+#' @import ggrepel
+#' @import signal
+#' @import tidyverse
+#' @import crayon
+#' @import pbmcapply
+#' @import plotly
+#' @import mstools
+#' @import zoo
+#' @import dbplyr
+#' @import tidyr
+#' @import mzR
+#' @import Rcpp
+#' @import DIAlignR
+#' @import LatticeExtra
+#'
+#' @source "GetXIC.r"
+#' @source "getPepLibData.R"
+#' @source "getChromatogramDatapoints.R"
+#' @source "plot_aligned.R"
+#' @source "plot_chrom_reference.R"
+#' 
+#' @export
 
 
 runDrawAlignR <- function() {
